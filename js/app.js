b961cfdcc0c2008acc614c873488585a0dd7b9fb
Albertoice <alberto@MacBook-Air-di-Alberto.local>
2020-01-15 16:57:06 +0100

    primo salvataggio

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var input = document.querySelector('.input');
+var output = document.querySelector('.output');
+var btn = document.querySelector('.btn');
+
+function generaAlunnoCasuale() {
+    var numeroAlunni = input.value;
+    var numeroCasuale = Math.floor(Math.random() * numeroAlunni + 1);
+    output.innerHTML = numeroCasuale;
+}
+
+btn.addEventListener('click', generaAlunnoCasuale);
